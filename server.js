689e40b90c4ac321276f13305199fcf02b692753
Christian <chriveib@stud.ntnu.no>
2023-12-19 14:00:15 +0100

    Moced dist folder #3

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const path = require("path");
 const app = express();
 
 // Define your routes and middleware here
-app.use(express.static(path.join(__dirname, "./client", "dist")));
+app.use(express.static(path.join(__dirname, "dist")));
 // Handle all other GET-reqs
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 // Start the server
